8bfbb53a3fe600eb9a5569fa48461c562c4ec3ec
FatimaGhafoor <channu82411@gmail.com>
2026-06-11 15:43:00 +0500

    refactor: simplify app.js to single entry point using WeatherController

--- js/app.js
+++ js/app.js
@@ -1,33 +1,7 @@
-import { createWeatherController } from "./weatherController.js";
-import { fetchWeather } from "./weatherService.js";
-import {
-  getDOMElements,
-  showMessage,
-  showLoading,
-  displayWeather,
-} from "./weatherView.js";
-import { getCache, setCache } from "./cacheService.js";
-import { CONFIG } from "./config.js";
+import { WeatherController } from "./weatherController.js";
 
-const elements = getDOMElements();
-
-const services = {
-  fetchWeather,
-  cache: { get: getCache, set: setCache },
-};
-
-const view = {
-  showMessage,
-  showLoading,
-  displayWeather,
-};
-
-const controller = createWeatherController({
-  elements,
-  services,
-  view,
-  config: CONFIG,
-});
-
-controller.setupEventListeners();
-controller.setupNetworkListeners();
+try {
+  new WeatherController().init();
+} catch (error) {
+  console.error("App initialization failed:", error);
+}
